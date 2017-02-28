4dd25994985203ba5d1217e5fc3f2d1b469b3272
Michael Ritter <shake@umiacs.umd.edu>
2017-02-28 16:27:39 -0500

    Refactor RepairScheduler to use a Executor and minimize code dupe

--- src/main/java/org/chronopolis/medic/scheduled/RepairScheduler.java
+++ src/main/java/org/chronopolis/medic/scheduled/RepairScheduler.java
@@ -1,12 +1,15 @@
 package org.chronopolis.medic.scheduled;
 
 import com.google.common.collect.ImmutableMap;
+import org.chronopolis.common.concurrent.TrackingThreadPoolExecutor;
 import org.chronopolis.medic.OptionalCallback;
 import org.chronopolis.medic.client.RepairManager;
 import org.chronopolis.medic.client.Repairs;
 import org.chronopolis.medic.config.IngestConfiguration;
-import org.chronopolis.rest.models.repair.Fulfillment;
-import org.chronopolis.rest.models.repair.FulfillmentStatus;
+import org.chronopolis.medic.runners.RepairBackup;
+import org.chronopolis.medic.runners.RepairCleaner;
+import org.chronopolis.medic.runners.RepairReplicator;
+import org.chronopolis.medic.runners.RepairValidator;
 import org.chronopolis.rest.models.repair.Repair;
 import org.chronopolis.rest.models.repair.RepairStatus;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +18,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import retrofit2.Call;
-import retrofit2.Response;
 
-import java.io.IOException;
 import java.util.Optional;
 
 /**
+ * Scheduled tasks which query the repair api for pending operations
  *
  * Created by shake on 2/16/17.
  */
@@ -31,74 +33,78 @@ public class RepairScheduler {
     private final Repairs repairs;
     private final RepairManager manager;
     private final IngestConfiguration configuration;
+    private final TrackingThreadPoolExecutor<Repair> executor;
 
     @Autowired
-    public RepairScheduler(Repairs repairs, RepairManager manager, IngestConfiguration configuration) {
+    public RepairScheduler(Repairs repairs, RepairManager manager, IngestConfiguration configuration, TrackingThreadPoolExecutor<Repair> executor) {
         this.repairs = repairs;
         this.manager = manager;
         this.configuration = configuration;
+        this.executor = executor;
     }
+
+    /**
+     * Query the repairs api with the requested parameter
+     *
+     * @param params the query parameters
+     * @return a response object if available
+     */
+    private Optional<Page<Repair>> getRepairs(ImmutableMap<String, String> params) {
+        Call<Page<Repair>> reapers = repairs.getRepairs(params);
+        OptionalCallback<Page<Repair>> cb = new OptionalCallback<>();
+        reapers.enqueue(cb);
+        return cb.get();
+    }
+
+    // Scheduled Tasks to run query and start threads
 
     @Scheduled
     public void backup() {
-        final String node = configuration.getUsername();
-        Call<Page<Repair>> call = repairs.getRepairs(ImmutableMap.of("to", node, "backup", String.valueOf(false)));
-
-        // Would be nice to trim this down a little bit (always new cb, enqueue, get), maybe there's something we can do
-        OptionalCallback<Page<Repair>> cb = new OptionalCallback<>();
-        call.enqueue(cb);
-        Optional<Page<Repair>> repairs = cb.get();
-        repairs.ifPresent(this::doBackup);
-
-    }
-
-    private void doBackup(Page<Repair> repairs) {
-        repairs.forEach(manager::backup);
+        ImmutableMap<String, String> params = ImmutableMap.of("to", configuration.getUsername(),
+                "backup", String.valueOf(false));
+        getRepairs(params).ifPresent(this::doBackup);
     }
 
     @Scheduled
     public void repair() {
-        final String node = configuration.getUsername();
-        Call<Page<Repair>> reapers = repairs.getRepairs(ImmutableMap.of("to", node, "status", RepairStatus.FULFILLING.toString()));
-        try {
-            Response<Page<Repair>> execute = reapers.execute();
-            if (execute.isSuccessful()) {
-                Page<Repair> body = execute.body();
-                body.forEach(this::submit);
-            }
-        } catch (IOException e) {
-            // blah blah
-
-        }
-    }
-
-    private void submit(Repair repair) {
-        manager.backup(repair);
-        if (repair.getStatus() == RepairStatus.FULFILLING) {
-            OptionalCallback<Fulfillment> opFulfillment = new OptionalCallback<>();
-            Call<Fulfillment> call = repairs.getFulfillment(repair.getFulfillment());
-            call.enqueue(opFulfillment);
-
-            Optional<Fulfillment> fulfillment = opFulfillment.get();
-            fulfillment.filter(f -> f.getStatus() == FulfillmentStatus.READY)
-                    .ifPresent(manager::replicate);
-        }
+        ImmutableMap<String, String> params = ImmutableMap.of("to", configuration.getUsername(),
+                "backup", String.valueOf(true),                 // only attempt repairs which are already backed up
+                "status", RepairStatus.FULFILLING.toString());
+        getRepairs(params).ifPresent(this::submit);
     }
 
     @Scheduled
     public void audit() {
-    }
-
-    private void runAudit(Page<Repair> repairs) {
-        repairs.forEach(manager::validateFiles);
+        ImmutableMap<String, String> params = ImmutableMap.of("to", configuration.getUsername(),
+                "backup", String.valueOf(true),                 // only attempt repairs which are already backed up
+                "status", RepairStatus.FULFILLING.toString());
+        getRepairs(params).ifPresent(this::runAudit);
     }
 
     @Scheduled
     public void clean() {
+        ImmutableMap<String, String> params = ImmutableMap.of("to", configuration.getUsername(),
+                "backup", String.valueOf(false),
+                "status", RepairStatus.REPAIRED.toString());
+        getRepairs(params).ifPresent(this::runClean);
     }
 
-    private void runClean(Page<Repair> repairs) {
-        repairs.forEach(manager::removeBackup);
+    // Helpers to submit threads to our executor
+
+    private void doBackup(Page<Repair> repairPage) {
+        repairPage.forEach(repair -> executor.submitIfAvailable(new RepairBackup(repair, repairs, manager), repair));
+    }
+
+    private void submit(Page<Repair> repairPage) {
+        repairPage.forEach(repair -> executor.submitIfAvailable(new RepairReplicator(repair, repairs, manager), repair));
+    }
+
+    private void runAudit(Page<Repair> repairPage) {
+        repairPage.forEach(repair -> executor.submitIfAvailable(new RepairValidator(repair, repairs, manager), repair));
+    }
+
+    private void runClean(Page<Repair> repairPage) {
+        repairPage.forEach(repair -> executor.submitIfAvailable(new RepairCleaner(repair, repairs, manager), repair));
     }
 
 }
