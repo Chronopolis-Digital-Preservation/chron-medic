f039bef4b603bdf08b9c8f421ad53710d183022b
Michael Ritter <shake@umiacs.umd.edu>
2017-03-13 16:48:17 -0400

    Introduce enum to encapsulate the results of a Comparison beyond just true/false

---
+++ src/main/java/org/chronopolis/medic/client/CompareResult.java
@@ -0,0 +1,10 @@
+package org.chronopolis.medic.client;
+
+/**
+ * Some results about ACE collection repairs
+ *
+ * Created by shake on 3/13/17.
+ */
+public enum CompareResult {
+    VALID, INVALID, CONNECTION_ERROR
+}
