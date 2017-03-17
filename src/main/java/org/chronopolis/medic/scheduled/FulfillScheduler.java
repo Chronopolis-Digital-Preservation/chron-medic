6f0de399a0bac846b88e9351d4cf61cc380da6dc
Michael Ritter <shake@umiacs.umd.edu>
2017-03-17 14:25:38 -0400

    Extend our superclass;  Add cron timers

--- src/main/java/org/chronopolis/medic/scheduled/FulfillScheduler.java
+++ src/main/java/org/chronopolis/medic/scheduled/FulfillScheduler.java
@@ -2,7 +2,6 @@ package org.chronopolis.medic.scheduled;
 
 import com.google.common.collect.ImmutableMap;
 import org.chronopolis.common.concurrent.TrackingThreadPoolExecutor;
-import org.chronopolis.medic.OptionalCallback;
 import org.chronopolis.medic.client.Repairs;
 import org.chronopolis.medic.client.StageManager;
 import org.chronopolis.medic.config.IngestConfiguration;
@@ -15,66 +14,52 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import retrofit2.Call;
-
-import java.util.Optional;
 
 /**
+ * Fulfillment tasks which are run. Consists of staging and cleaning.
  *
  * Created by shake on 2/16/17.
  */
 @Profile("fulfill")
 @EnableScheduling
-public class FulfillScheduler {
+public class FulfillScheduler extends Scheduler<Fulfillment> {
 
     private final Repairs repairs;
     private final StageManager manager;
     private final IngestConfiguration configuration;
-    private final TrackingThreadPoolExecutor<Fulfillment> executor;
 
     @Autowired
     public FulfillScheduler(Repairs repairs, StageManager manager, IngestConfiguration configuration, TrackingThreadPoolExecutor<Fulfillment> executor) {
+        super(executor);
         this.repairs = repairs;
         this.manager = manager;
         this.configuration = configuration;
-        this.executor = executor;
     }
 
-    @Scheduled
+    @Scheduled(cron = "${cron.fulfillment:0 0 * * * * }")
     public void fulfill() {
         String node = configuration.getUsername();
-        Call<Page<Fulfillment>> call = repairs.getFulfillments(ImmutableMap.of(
-                "from", node,
-                "status", FulfillmentStatus.STAGING.toString()));
-
-        OptionalCallback<Page<Fulfillment>> cb = new OptionalCallback<>();
-        call.enqueue(cb);
-
-        Optional<Page<Fulfillment>> fulfillments = cb.get();
-        fulfillments.ifPresent(this::submit);
+        ImmutableMap<String, String> params = ImmutableMap.of("from", node,
+                "status", FulfillmentStatus.STAGING.toString());
+        get(repairs::getFulfillments, params).ifPresent(this::submit);
     }
 
-    @Scheduled
+    @Scheduled(cron = "${cron.fulfillment:0 0 * * * * }")
     public void clean() {
         String node = configuration.getUsername();
-        Call<Page<Fulfillment>> call = repairs.getFulfillments(ImmutableMap.of(
+        ImmutableMap<String, String> params = ImmutableMap.of(
                 "from", node,
-                "status", FulfillmentStatus.COMPLETE.toString(),
-                "cleaned", String.valueOf(false)));
-
-        OptionalCallback<Page<Fulfillment>> cb = new OptionalCallback<>();
-        call.enqueue(cb);
-
-        Optional<Page<Fulfillment>> fulfillments = cb.get();
-        fulfillments.ifPresent(this::submitForClean);
+                "status", FulfillmentStatus.COMPLETE.toString(), // todo: also check failure
+                "cleaned", String.valueOf(false));
+        get(repairs::getFulfillments, params).ifPresent(this::submitForClean);
     }
 
     private void submitForClean(Page<Fulfillment> fulfillments) {
-        fulfillments.forEach(fulfillment -> executor.submitIfAvailable(new FulfillmentCleaner(repairs, manager, fulfillment), fulfillment));
+        fulfillments.forEach(fulfillment -> submit(fulfillment, new FulfillmentCleaner(repairs, manager, fulfillment)));
     }
 
     private void submit(Page<Fulfillment> fulfillments) {
-        fulfillments.forEach(fulfillment -> executor.submitIfAvailable(new FulfillmentStager(repairs, manager, fulfillment), fulfillment));
+        fulfillments.forEach(fulfillment -> submit(fulfillment, new FulfillmentStager(repairs, manager, fulfillment)));
     }
 
 }
--- src/main/java/org/chronopolis/medic/scheduled/RepairScheduler.java
+++ src/main/java/org/chronopolis/medic/scheduled/RepairScheduler.java
@@ -2,24 +2,24 @@ package org.chronopolis.medic.scheduled;
 
 import com.google.common.collect.ImmutableMap;
 import org.chronopolis.common.concurrent.TrackingThreadPoolExecutor;
-import org.chronopolis.medic.OptionalCallback;
 import org.chronopolis.medic.client.RepairManager;
 import org.chronopolis.medic.client.Repairs;
 import org.chronopolis.medic.config.IngestConfiguration;
 import org.chronopolis.medic.runners.RepairAuditor;
 import org.chronopolis.medic.runners.RepairCleaner;
+import org.chronopolis.medic.runners.RepairCopier;
 import org.chronopolis.medic.runners.RepairReplicator;
+import org.chronopolis.medic.runners.RepairValidator;
 import org.chronopolis.rest.models.repair.FulfillmentStatus;
 import org.chronopolis.rest.models.repair.Repair;
 import org.chronopolis.rest.models.repair.RepairStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import retrofit2.Call;
-
-import java.util.Optional;
 
 /**
  * Descending through dimensions darkened by dementia
@@ -29,84 +29,104 @@ import java.util.Optional;
  */
 @Profile("repair")
 @EnableScheduling
-public class RepairScheduler {
+public class RepairScheduler extends Scheduler<Repair> {
+    private final Logger log = LoggerFactory.getLogger(RepairScheduler.class);
 
     private final Repairs repairs;
     private final RepairManager manager;
     private final IngestConfiguration configuration;
-    private final TrackingThreadPoolExecutor<Repair> executor;
 
     @Autowired
-    public RepairScheduler(Repairs repairs, RepairManager manager, IngestConfiguration configuration, TrackingThreadPoolExecutor<Repair> executor) {
+    public RepairScheduler(Repairs repairs,
+                           RepairManager manager,
+                           IngestConfiguration configuration,
+                           TrackingThreadPoolExecutor<Repair> repairPool) {
+        super(repairPool);
         this.repairs = repairs;
         this.manager = manager;
         this.configuration = configuration;
-        this.executor = executor;
-    }
-
-    /**
-     * Query the repairs api with the requested parameter
-     *
-     * @param params the query parameters
-     * @return a response object if available
-     */
-    private Optional<Page<Repair>> getRepairs(ImmutableMap<String, String> params) {
-        Call<Page<Repair>> reapers = repairs.getRepairs(params);
-        OptionalCallback<Page<Repair>> cb = new OptionalCallback<>();
-        reapers.enqueue(cb);
-        return cb.get();
     }
 
     // Scheduled Tasks to run query and start threads
 
-    @Scheduled
+    @Scheduled(cron = "${cron.repair:0 0 * * * * }")
     public void repair() {
-        // todo: query on fulfillment
         ImmutableMap<String, String> params = ImmutableMap.of("to", configuration.getUsername(),
-                "replaced", String.valueOf(false),
-                "status", RepairStatus.FULFILLING.toString());
-        getRepairs(params).ifPresent(this::replicate);
+                "fulfillment-status", FulfillmentStatus.READY.toString());
+        get(repairs::getRepairs, params).ifPresent(this::replicate);
     }
 
-    @Scheduled
+
+    @Scheduled(cron = "${cron.repair:0 0 * * * * }")
     public void validate() {
-        // todo: query on fulfillments
         ImmutableMap<String, String> params = ImmutableMap.of("to", configuration.getUsername(),
-                "validated", String.valueOf(false),
-                "status", FulfillmentStatus.TRANSFERRED.toString());
-        getRepairs(params).ifPresent(this::validate);
+                "fulfillment-validated", String.valueOf(false),
+                "fulfillment-status", FulfillmentStatus.TRANSFERRED.toString());
+        get(repairs::getRepairs, params).ifPresent(this::runValidate);
     }
 
-    @Scheduled
+    @Scheduled(cron = "${cron.repair:0 0 * * * * }")
+    public void copy() {
+        ImmutableMap<String, String> params = ImmutableMap.of("to", configuration.getUsername(),
+                "fulfillment-validated", String.valueOf(true),
+                "fulfillment-status", FulfillmentStatus.TRANSFERRED.toString());
+        get(repairs::getRepairs, params).ifPresent(this::runCopy);
+    }
+
+
+    @Scheduled(cron = "${cron.repair:0 0 * * * * }")
     public void audit() {
         ImmutableMap<String, String> params = ImmutableMap.of("to", configuration.getUsername(),
                 "replaced", String.valueOf(true),
                 "status", RepairStatus.FULFILLING.toString());
-        getRepairs(params).ifPresent(this::runAudit);
+        get(repairs::getRepairs, params).ifPresent(this::runAudit);
     }
 
-    @Scheduled
+
+    @Scheduled(cron = "${cron.repair:0 0 * * * * }")
     public void clean() {
         ImmutableMap<String, String> params = ImmutableMap.of("to", configuration.getUsername(),
                 "cleaned", String.valueOf(false),
                 "status", RepairStatus.REPAIRED.toString()); // repaired or failed
-        getRepairs(params).ifPresent(this::runClean);
+        get(repairs::getRepairs, params).ifPresent(this::runClean);
     }
 
-    // Helpers to submit threads to our executor
 
-    private void replicate(Page<Repair> repairPage) {
-        repairPage.forEach(repair -> executor.submitIfAvailable(new RepairReplicator(repair, repairs, manager), repair));
+    // Helpers to submit threads to our executor. Maybe some way to trim a bit of the fat, but that's not a big concern atm.
+
+    /*
+     * An example of how we might cut down on some of the boilerplate. The only issue being we should really do Class<? extends RepairRunner>
+     * so that we can ensure a constructor of Repair, Repairs, RepairManager exists (though I don't know if that would be enough).
+     *
+    private void submit(Page<Repair> page, Class<? extends Runnable> clazz) {
+        page.forEach(repair -> {
+            try {
+                Constructor<? extends Runnable> constructor = clazz.getConstructor(Repair.class, Repairs.class, RepairManager.class);
+                submit(repair, constructor.newInstance(repair, repairs, manager));
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                log.warn("{} error in submission", repair.getCollection(), e);
+            }
+        });
+    }
+    */
+
+    private void replicate(Page<Repair> page) {
+        page.forEach(repair -> submit(repair, new RepairReplicator(repair, repairs, manager)));
+    }
+
+    private void runValidate(Page<Repair> page) {
+        page.forEach(repair -> submit(repair, new RepairValidator(repair, repairs, manager)));
+    }
+
+    private void runCopy(Page<Repair> page) {
+        page.forEach(repair -> submit(repair, new RepairCopier(repair, repairs, manager)));
     }
 
     private void runAudit(Page<Repair> repairPage) {
-        repairPage.forEach(repair -> executor.submitIfAvailable(new RepairAuditor(repair, repairs, manager), repair));
+        repairPage.forEach(repair -> submit(repair, new RepairAuditor(repair, repairs, manager)));
     }
 
     private void runClean(Page<Repair> repairPage) {
-        repairPage.forEach(repair -> executor.submitIfAvailable(new RepairCleaner(repair, repairs, manager), repair));
-    }
-
-    private void validate(Page<Repair> repairPage) {
+        repairPage.forEach(repair -> submit(repair, new RepairCleaner(repair, repairs, manager)));
     }
 }
