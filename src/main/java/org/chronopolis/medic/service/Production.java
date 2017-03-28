61a01b757b994e85de12367b9b342cc48c736449
Michael Ritter <shake@umiacs.umd.edu>
2017-03-28 16:01:30 -0400

    Implement our Production service runner

--- src/main/java/org/chronopolis/medic/service/Production.java
+++ src/main/java/org/chronopolis/medic/service/Production.java
@@ -1,7 +1,15 @@
 package org.chronopolis.medic.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Production runner which sleeps eternally
@@ -11,8 +19,34 @@ import org.springframework.stereotype.Component;
 @Component
 @Profile("default")
 public class Production implements Service {
+
+    private final Logger log = LoggerFactory.getLogger(Production.class);
+
+    private final ApplicationContext context;
+    private AtomicBoolean running;
+
+    @Autowired
+    public Production(ApplicationContext context) {
+        this.context = context;
+        this.running = new AtomicBoolean(true);
+    }
+
     @Override
     public void run() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            running.set(false);
+        }));
 
+        while (running.get()) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(500);
+            } catch (InterruptedException e) {
+                log.info("Interrupted");
+                SpringApplication.exit(context);
+            }
+        }
+
+        SpringApplication.exit(context);
     }
+
 }
